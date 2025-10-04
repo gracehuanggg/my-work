93bd7e32db4fa6088e46cab3c0c3ef0924a57d87
Grace Huang <ghuang26@nmhschool.org>
2025-10-04 09:51:20 -0400

    Update shapes.py

--- day_03/modular_code/Shape Toolkit Project/shapes.py
+++ day_03/modular_code/Shape Toolkit Project/shapes.py
@@ -6,12 +6,6 @@ class Rectangle:
         return self.width * self.height
     def describe(self):
         print(f"Rectangle {self.width} by {self.height} has area {self.area()} cm^2")
-rect_1 = Rectangle (10,5)
-rect_1.describe()
-print (f"Area of rectangle is {rect_1.area()} cm^2")
-rect_2 = Rectangle (3,7)
-rect_2.describe()
-print (f"Area of rectangle is {rect_2.area()} cm^2")
 
 class circle:
     def __init__(self, radius):
@@ -21,12 +15,6 @@ class circle:
         return self.radius * self.radius * self.pi
     def describe(self):
         print (f"Circle with radius {self.radius} has area {self.area()} cm^2")
-circle_1 = circle (5)
-circle_1.describe()
-print (f"Area of circle is {circle_1.area()} cm^2")
-circle_2 = circle (10)
-circle_2.describe()
-print (f"Area of circle is {circle_2.area()} cm^2")
 
 class triangle:
     def __init__(self, base, height):
@@ -36,9 +24,3 @@ class triangle:
         return 0.5 * self.base * self.height
     def describe(self):
         print (f"Triangle with base {self.base} and height {self.height} has area {self.area()} cm^2")
-triangle_1 = triangle (10,5)
-triangle_1.describe()
-print (f"Area of triangle is {triangle_1.area()} cm^2")
-triangle_2 = triangle (3,7)
-triangle_2.describe()
-print (f"Area of triangle is {triangle_2.area()} cm^2")

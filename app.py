e8bdd98bdb3367481f2407afa185476f05892219
Grace Huang <ghuang26@nmhschool.org>
2025-11-03 14:03:24 -0500

    hw1 for front end

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import jinja2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/contact")
+def contact():
+    email = "ghuang26@nmhschool.org"
+    phone = "857-500-3746"
+    is_student = True
+    return render_template("contact.html", email=email, is_student=is_student, phone=phone)
+    
+
+@app.route("/about")
+def about():
+    author = "Grace Huang"
+    interests = ["Making wesbites", "Coding", "Web browsing"]
+    return render_template("about.html", author=author, interests=interests)
+
+app.run(debug=True)
